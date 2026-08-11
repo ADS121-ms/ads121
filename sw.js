4dceaa5947f2fc82ca74c4e6581111c0b66708ce
ADS121-ms <315230440+ADS121-ms@users.noreply.github.com>
2026-08-12 01:39:41 +0800

    新版
    新版

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='timeplan-v1';
+const CACHE='timeplan-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
