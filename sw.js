1971896e684057807d03926f9d46b5acfa0ac1e9
ADS121-ms <315230440+ADS121-ms@users.noreply.github.com>
2026-08-12 00:22:26 +0800

    首次部署
    首次部署

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='timeplan-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
